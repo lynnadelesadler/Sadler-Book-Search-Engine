8c999af6647d320811d35d5f9eaf660261940bb2
lynnadelesadler <lynnadelesadler@yahoo.com>
2023-02-22 22:13:28 -0500

    Replace the loginUser imported from the API file with the LOGIN_USER mutation

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,7 +1,8 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
@@ -26,24 +27,20 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await login({
+        variables: { ...userFormData },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
     }
 
     setUserFormData({
-      username: '',
-      email: '',
-      password: '',
+      username: "",
+      email: "",
+      password: "",
     });
   };
 
